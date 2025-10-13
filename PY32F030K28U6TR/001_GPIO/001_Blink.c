ec00106a2c145817a47924294a5eb2a15d3360b6
unknown <skuratowaanna@yandex.ru>
2025-10-13 23:57:32 +0300

    gpio cmsis

--- PY32F030K28U6TR/001_GPIO/001_Blink.c
+++ PY32F030K28U6TR/001_GPIO/001_Blink.c
@@ -136,3 +136,94 @@ void assert_failed(uint8_t *file, uint32_t line)
 }
 #endif
 /*****************************END OF FILE**************************************/
+/*
+ *  PY32F030K28U6TR
+ *   ------------
+ *  |            |
+ *  |            |
+ *  |            |
+ *  |       PA.2 | ---->  LED
+ *  |       PA.3 | ---->  LED
+ *  |       PA.4 | ---->  LED
+ *  |            |
+ *  |            |
+ *  |      +3.3V |
+ *  |        GND |
+ *
+ */
+/****************************** The third example *****************************/
+#include "py32f030x8.h"
+
+#define SET_BIT(REG, BIT) ((REG) |= (BIT))
+#define CLEAR_BIT(REG, BIT) ((REG) &= ~(BIT))
+#define READ_BIT(REG, BIT) ((REG) & (BIT))
+#define CLEAR_REG(REG) ((REG) = (0x0))
+#define WRITE_REG(REG, VAL) ((REG) = (VAL))
+#define READ_REG(REG) ((REG))
+#define MODIFY_REG(REG, CLEARMASK, SETMASK) WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+
+#define Input_mode                  (0x0UL)
+#define General_purpose_output_mode (0x1UL)
+#define Alternate_function_mode     (0x2UL)
+#define Analog_mode                 (0x3UL)
+
+#define Output_push_pull            (0x0UL)
+#define Output_open_drain           (0x1UL)
+
+#define Very_low_speed              (0x0UL)
+#define Low_speed                   (0x1UL)
+#define High_speed                  (0x2UL)
+#define Very_high_speed             (0x3UL)
+
+#define No_pull_up_No_pull_down     (0x0UL)
+#define Pull_up                     (0x1UL)
+#define Pull_down                   (0x2UL)
+
+void PORTA_2_3_4_INIT(void);
+
+// --- ПРОСТАЯ ФУНКЦИЯ ЗАДЕРЖКИ (DELAY) ---
+void delay_ms(uint32_t ms) {
+  for (uint32_t i = 0; i < ms * 1000; i++) {
+    __NOP(); // No operation instruction
+  }
+}
+
+int main(void) {
+PORTA_2_3_4_INIT();
+  while (1) {
+    SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS2);
+    SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS3);
+    SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS4);
+    delay_ms(300);
+    SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR2);
+    SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR3);
+    SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR4);
+    delay_ms(300);
+  }
+}
+
+void PORTA_2_3_4_INIT(void) {
+  // ===================================================================
+  // 1. НАСТРОЙКА ТАКТИРОВАНИЯ (Clock Setup)
+  // ===================================================================
+  // Включение тактирования для порта GPIOA.
+  SET_BIT(RCC->IOPENR, RCC_IOPENR_GPIOAEN);
+  // ===================================================================
+  // 2. НАСТРОЙКА GPIO (Pin Configuration)
+  // ===================================================================
+  MODIFY_REG(GPIOA->MODER, GPIO_MODER_MODE2, General_purpose_output_mode << GPIO_MODER_MODE2_Pos); // Настройка GPIOA пин 2 на выход (output mode)
+  MODIFY_REG(GPIOA->OTYPER, GPIO_OTYPER_OT2, Output_push_pull << GPIO_OTYPER_OT2_Pos);             // Настройка GPIOA пин 2 в режим Push-Pull
+  MODIFY_REG(GPIOA->OSPEEDR, GPIO_OSPEEDR_OSPEED2, High_speed << GPIO_OSPEEDR_OSPEED2_Pos);        // Настройка GPIOA пин 2 в режим High_speed
+  MODIFY_REG(GPIOA->PUPDR, GPIO_PUPDR_PUPD2, No_pull_up_No_pull_down << GPIO_PUPDR_PUPD2_Pos);     // Настройка GPIOA пин 2 в режим No_pull_up_No_pull_down
+  // ===================================================================
+  MODIFY_REG(GPIOA->MODER, GPIO_MODER_MODE3, General_purpose_output_mode << GPIO_MODER_MODE3_Pos); // Настройка GPIOA пин 3 на выход (output mode)
+  MODIFY_REG(GPIOA->OTYPER, GPIO_OTYPER_OT3, Output_push_pull << GPIO_OTYPER_OT3_Pos);             // Настройка GPIOA пин 3 в режим Push-Pull
+  MODIFY_REG(GPIOA->OSPEEDR, GPIO_OSPEEDR_OSPEED3, High_speed << GPIO_OSPEEDR_OSPEED3_Pos);        // Настройка GPIOA пин 3 в режим High_speed
+  MODIFY_REG(GPIOA->PUPDR, GPIO_PUPDR_PUPD3, No_pull_up_No_pull_down << GPIO_PUPDR_PUPD3_Pos);     // Настройка GPIOA пин 3 в режим No_pull_up_No_pull_down
+  // ===================================================================
+  MODIFY_REG(GPIOA->MODER, GPIO_MODER_MODE4, General_purpose_output_mode << GPIO_MODER_MODE4_Pos); // Настройка GPIOA пин 4 на выход (output mode)
+  MODIFY_REG(GPIOA->OTYPER, GPIO_OTYPER_OT4, Output_push_pull << GPIO_OTYPER_OT4_Pos);             // Настройка GPIOA пин 4 в режим Push-Pull
+  MODIFY_REG(GPIOA->OSPEEDR, GPIO_OSPEEDR_OSPEED4, High_speed << GPIO_OSPEEDR_OSPEED4_Pos);        // Настройка GPIOA пин 4 в режим High_speed
+  MODIFY_REG(GPIOA->PUPDR, GPIO_PUPDR_PUPD4, No_pull_up_No_pull_down << GPIO_PUPDR_PUPD4_Pos);     // Настройка GPIOA пин 4 в режим No_pull_up_No_pull_down
+}
+/*****************************END OF FILE**************************************/
--- PY32F030K28U6TR/001_GPIO/002_Button_Led.c
+++ PY32F030K28U6TR/001_GPIO/002_Button_Led.c
@@ -157,3 +157,103 @@ void assert_failed(uint8_t *file, uint32_t line)
 }
 #endif
 /*****************************END OF FILE**************************************/
+/*
+ *  PY32F030K28U6TR
+ *   ------------
+ *  |            |
+ *  |            |
+ *  |            |
+ *  |       PA.2 | ---->  LED
+ *  |       PA.3 | ---->  LED
+ *  |       PA.4 | ---->  LED
+ *  |            |
+ *  |       PB.7 | <----  Button
+ *  |            |
+ *  |      +3.3V |
+ *  |        GND |
+ *
+ */
+/****************************** The third example *****************************/
+#include "py32f030x8.h"
+
+#define SET_BIT(REG, BIT) ((REG) |= (BIT))
+#define CLEAR_BIT(REG, BIT) ((REG) &= ~(BIT))
+#define READ_BIT(REG, BIT) ((REG) & (BIT))
+#define CLEAR_REG(REG) ((REG) = (0x0))
+#define WRITE_REG(REG, VAL) ((REG) = (VAL))
+#define READ_REG(REG) ((REG))
+#define MODIFY_REG(REG, CLEARMASK, SETMASK) WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+
+#define GPIO_Pin_0  ((uint16_t)0x0001)
+#define GPIO_Pin_1  ((uint16_t)0x0002)
+#define GPIO_Pin_2  ((uint16_t)0x0004)
+#define GPIO_Pin_3  ((uint16_t)0x0008)
+#define GPIO_Pin_4  ((uint16_t)0x0010)
+#define GPIO_Pin_5  ((uint16_t)0x0020)
+#define GPIO_Pin_6  ((uint16_t)0x0040)
+#define GPIO_Pin_7  ((uint16_t)0x0080)
+#define GPIO_Pin_8  ((uint16_t)0x0100)
+#define GPIO_Pin_9  ((uint16_t)0x0200)
+#define GPIO_Pin_10 ((uint16_t)0x0400)
+#define GPIO_Pin_11 ((uint16_t)0x0800)
+#define GPIO_Pin_12 ((uint16_t)0x1000)
+#define GPIO_Pin_13 ((uint16_t)0x2000)
+#define GPIO_Pin_14 ((uint16_t)0x4000)
+#define GPIO_Pin_15 ((uint16_t)0x8000)
+
+#define Input_mode (0x0UL)
+#define General_purpose_output_mode (0x1UL)
+#define Alternate_function_mode (0x2UL)
+#define Analog_mode (0x3UL)
+
+#define Output_push_pull (0x0UL)
+#define Output_open_drain (0x1UL)
+
+#define Very_low_speed (0x0UL)
+#define Low_speed (0x1UL)
+#define High_speed (0x2UL)
+#define Very_high_speed (0x3UL)
+
+#define No_pull_up_No_pull_down (0x0UL)
+#define Pull_up (0x1UL)
+#define Pull_down (0x2UL)
+
+void Led_PORTA_2_INIT(void);
+void Button_PORTB_7_INIT(void);
+
+int main(void)
+{
+  Led_PORTA_2_INIT();
+  Button_PORTB_7_INIT();
+  while (1)
+  {
+    ((GPIOB->IDR & GPIO_Pin_7)==0x0) ? (SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS2|GPIO_BSRR_BS3|GPIO_BSRR_BS4)) : (SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR2|GPIO_BSRR_BR3|GPIO_BSRR_BR4));
+  }
+}
+
+void Led_PORTA_2_INIT(void)
+{
+  SET_BIT(RCC->IOPENR, RCC_IOPENR_GPIOAEN);
+  MODIFY_REG(GPIOA->MODER, GPIO_MODER_MODE2, General_purpose_output_mode << GPIO_MODER_MODE2_Pos); // Настройка GPIOA пин 2 на выход (output mode)
+  MODIFY_REG(GPIOA->OTYPER, GPIO_OTYPER_OT2, Output_push_pull << GPIO_OTYPER_OT2_Pos);             // Настройка GPIOA пин 2 в режим Push-Pull
+  MODIFY_REG(GPIOA->OSPEEDR, GPIO_OSPEEDR_OSPEED2, High_speed << GPIO_OSPEEDR_OSPEED2_Pos);        // Настройка GPIOA пин 2 в режим High_speed
+  MODIFY_REG(GPIOA->PUPDR, GPIO_PUPDR_PUPD2, No_pull_up_No_pull_down << GPIO_PUPDR_PUPD2_Pos);     // Настройка GPIOA пин 2 в режим No_pull_up_No_pull_down
+  // ===================================================================
+  MODIFY_REG(GPIOA->MODER, GPIO_MODER_MODE3, General_purpose_output_mode << GPIO_MODER_MODE3_Pos); // Настройка GPIOA пин 3 на выход (output mode)
+  MODIFY_REG(GPIOA->OTYPER, GPIO_OTYPER_OT3, Output_push_pull << GPIO_OTYPER_OT3_Pos);             // Настройка GPIOA пин 3 в режим Push-Pull
+  MODIFY_REG(GPIOA->OSPEEDR, GPIO_OSPEEDR_OSPEED3, High_speed << GPIO_OSPEEDR_OSPEED3_Pos);        // Настройка GPIOA пин 3 в режим High_speed
+  MODIFY_REG(GPIOA->PUPDR, GPIO_PUPDR_PUPD3, No_pull_up_No_pull_down << GPIO_PUPDR_PUPD3_Pos);     // Настройка GPIOA пин 3 в режим No_pull_up_No_pull_down
+  // ===================================================================
+  MODIFY_REG(GPIOA->MODER, GPIO_MODER_MODE4, General_purpose_output_mode << GPIO_MODER_MODE4_Pos); // Настройка GPIOA пин 4 на выход (output mode)
+  MODIFY_REG(GPIOA->OTYPER, GPIO_OTYPER_OT4, Output_push_pull << GPIO_OTYPER_OT4_Pos);             // Настройка GPIOA пин 4 в режим Push-Pull
+  MODIFY_REG(GPIOA->OSPEEDR, GPIO_OSPEEDR_OSPEED4, High_speed << GPIO_OSPEEDR_OSPEED4_Pos);        // Настройка GPIOA пин 4 в режим High_speed
+  MODIFY_REG(GPIOA->PUPDR, GPIO_PUPDR_PUPD4, No_pull_up_No_pull_down << GPIO_PUPDR_PUPD4_Pos);     // Настройка GPIOA пин 4 в режим No_pull_up_No_pull_down
+}
+
+void Button_PORTB_7_INIT(void)
+{
+ SET_BIT(RCC->IOPENR, RCC_IOPENR_GPIOBEN);
+ MODIFY_REG(GPIOB->MODER, GPIO_MODER_MODE7, Input_mode << GPIO_MODER_MODE7_Pos);                  // Настройка GPIOB пин 7 на выход (Input_mode)
+ MODIFY_REG(GPIOB->PUPDR, GPIO_PUPDR_PUPD7, No_pull_up_No_pull_down << GPIO_PUPDR_PUPD7_Pos);     // Настройка GPIOB пин 7 в режим No_pull_up_No_pull_down
+}
+/*****************************END OF FILE**************************************/
